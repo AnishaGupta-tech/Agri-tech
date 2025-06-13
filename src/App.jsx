569473c55263769a24213a01b07710667c4f166d
Itisha-2006 <itishadas4301.9a@gmail.com>
2025-06-13 18:20:24 +0530

    imageupdated2

--- src/App.jsx
+++ src/App.jsx
@@ -204,7 +204,7 @@ const App = () => {
               <p>Receive real-time recommendations via app, SMS, or our voice assistant in your preferred language.</p>
             </div>
             <div className="step-image">
-              <img src="https://images.unsplash.com/photo-1581093450021-4a7360e9a7e0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt="Actionable insights" />
+              <img src="https://media.licdn.com/dms/image/v2/D5612AQFkwIWprwkR1g/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1663697058265?e=2147483647&v=beta&t=mpRRIczp7CUxH1zuD6KQxT8qf_VTj_ZLhK2KotJShV4" alt="Actionable insights" />
             </div>
           </div>
         </div>
